a736d34e07aafe8100ca328461d3d4146ab183dd
Nick Bircher <nicky.b.three@gmail.com>
2022-08-18 21:54:07 -0700

    Search + some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import React from 'react';
 
+const api = {
+  key: "09dc836f290099621cc439767c9a4f3c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-      <header>
-        Weather App
-      </header>
+    <div className='app'>
+      <main>
+        <div className='search-bar'>
+          <input
+            className='search-bar'
+            type='text'
+            placeholder='Search Cities'
+          />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Search = () => {
+
+
+    
+    return (
+    <div className='search-bar'>
+        <input
+        className='search-bar'
+        type='text'
+        placeholder='Search Cities'
+        />
+    </div>
+    );
+    
+}
